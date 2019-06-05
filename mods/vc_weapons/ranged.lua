97169c50583ab93786f46ba6c1202be0176ca281
LoneWolfHT <lonewolf04361@gmail.com>
2019-06-04 21:13:34 -0700

    Weapon tweaks

--- mods/vc_weapons/ranged.lua
+++ mods/vc_weapons/ranged.lua
@@ -1,13 +1,12 @@
 for gun, def in pairs(shooter.registered_weapons) do
 	if gun == "shooter_guns:pistol" then
 		def.spec.rounds = 10
-		def.spec.tool_caps = {full_punch_interval=0.25, damage_groups={fleshy=1}}
+		def.spec.tool_caps = {full_punch_interval=0.33, damage_groups={fleshy=1}}
 	elseif gun == "shooter_guns:rifle" then
-		def.spec.rounds = 20
-		def.spec.tool_caps = {full_punch_interval=0.9, damage_groups={fleshy=4}}
+		def.spec.rounds = 15
+		def.spec.tool_caps = {full_punch_interval=1, damage_groups={fleshy=4}}
 	elseif gun == "shooter_guns:shotgun" then
 		def.spec.rounds = 4
-		def.spec.spread = 20
 		def.spec.tool_caps = {full_punch_interval=1.5, damage_groups={fleshy=2}}
 	elseif gun == "shooter_guns:machine_gun" then
 		def.spec.rounds = 27
