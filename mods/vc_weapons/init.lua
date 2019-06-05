5d08be8a1350f7694bfe6d49ff7bddcf35ef3b17
LoneWolfHT <lonewolf04361@gmail.com>
2019-06-04 18:03:20 -0700

    Add a weapon mod

---
+++ mods/vc_weapons/init.lua
@@ -0,0 +1,4 @@
+local modpath = minetest.get_modpath("vc_weapons")
+
+dofile(modpath.."/ranged.lua")
+dofile(modpath.."/melee.lua")
---
+++ mods/vc_weapons/melee.lua
@@ -0,0 +1,23 @@
+minetest.register_tool("vc_weapons:knife", {
+	description = "Knife",
+	inventory_image = "weapons_knife.png",
+	range = 2,
+	wield_scale = {x = 0.7, y = 0.7, z = 0.7},
+	tool_capabilities = {
+		full_punch_interval = 0.9,
+		max_drop_level=0,
+		damage_groups = {fleshy=4},
+	},
+})
+
+minetest.register_tool("vc_weapons:sword", {
+	description = "Sword",
+	inventory_image = "weapons_sword.png",
+	wield_scale = {x = 1.3, y = 1.5, z = 1},
+	tool_capabilities = {
+		full_punch_interval = 0.8,
+		max_drop_level=1,
+		damage_groups = {fleshy=6},
+	},
+	sound = {breaks = "default_tool_breaks"},
+})
