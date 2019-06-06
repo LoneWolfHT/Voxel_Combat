a25f15babaf16696651cb8cd0f1e45a10a21f88a
LoneWolfHT <lonewolf04361@gmail.com>
2019-06-06 12:00:22 -0700

    Add trash in inv and more

--- mods/main/inv.lua
+++ mods/main/inv.lua
@@ -26,3 +26,21 @@ minetest.register_on_joinplayer(function(player)
 	player:hud_set_flags({healthbar = true, breathbar = true})
 
 end)
+
+sfinv.override_page("sfinv:crafting", {
+	title = "Main",
+	get = function(self, player, context)
+		return sfinv.make_formspec(player, context, [[
+				image[0,4.7;1,1;gui_hb_bg.png]
+				image[1,4.7;1,1;gui_hb_bg.png]
+				image[2,4.7;1,1;gui_hb_bg.png]
+				image[3,4.7;1,1;gui_hb_bg.png]
+				image[4,4.7;1,1;gui_hb_bg.png]
+				image[5,4.7;1,1;gui_hb_bg.png]
+				image[6,4.7;1,1;gui_hb_bg.png]
+				image[7,4.7;1,1;gui_hb_bg.png]
+				image[0.06,3.55;0.8,0.8;creative_trash_icon.png]
+				list[detached:creative_trash;main;0,3.45;1,1;]
+			]], true)
+	end
+})
