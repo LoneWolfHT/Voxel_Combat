eb24093f794b506479347d3a567f21f46bb49267
LoneWolfHT <lonewolf04361@gmail.com>
2019-06-06 11:54:09 -0700

    Kill falling players

---
+++ mods/main/kill_fall.lua
@@ -0,0 +1,14 @@
+local checkstep = 0
+minetest.register_globalstep(function(dtime)
+	if checkstep <= 5 then
+		checkstep = checkstep + dtime
+	else
+		checkstep = 0
+
+		for _, player in pairs(minetest.get_connected_players()) do
+			if player:get_pos().y < 1 then
+				player:set_hp(0, {reason = "set_hp"})
+			end
+		end
+	end
+end)
