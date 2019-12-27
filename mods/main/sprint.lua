28bca3bd9561301ecb06c0888172654576919444
LoneWolfHT <lonewolf04361@gmail.com>
2019-12-27 12:00:58 -0800

    Remove sprint and make players move slightly faster

--- mods/main/sprint.lua
+++ mods/main/sprint.lua
@@ -1,18 +1,24 @@
-local sprint_step = 0
-minetest.register_globalstep(function(dtime)
-	sprint_step = sprint_step + dtime
+minetest.register_on_joinplayer(function(p)
+	p:set_physics_override({speed = 1.5})
+end)
 
-	if sprint_step >= 0.35 then
-		sprint_step = 0
+--[[
+	local sprint_step = 0
+	minetest.register_globalstep(function(dtime)
+		sprint_step = sprint_step + dtime
 
-		for _, p in ipairs(minetest.get_connected_players()) do
-			if p:get_player_control().aux1 == true then
-				p:set_physics_override({speed = 2})
-			else
-				if p:get_physics_override().speed ~= 1 then
-					p:set_physics_override({speed = 1})
+		if sprint_step >= 0.35 then
+			sprint_step = 0
+
+			for _, p in ipairs(minetest.get_connected_players()) do
+				if p:get_player_control().aux1 == true then
+					p:set_physics_override({speed = 2})
+				else
+					if p:get_physics_override().speed ~= 1 then
+						p:set_physics_override({speed = 1})
+					end
 				end
 			end
 		end
-	end
-end)
+	end)
+]]
